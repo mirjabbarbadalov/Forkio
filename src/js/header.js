84f1fbf69ac40919eeb16804636475a6073236b1
Mirjabbar Badalov <mirishbedelov08@gmail.com>
2023-05-28 22:16:29 +0400

    header section added

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,21 @@
+const navButton = document.getElementById('open-menu')
+const headerNav = document.getElementById('header-nav')
+const navItem = document.querySelectorAll("#nav-item")
+const body = document.body
+
+navButton.addEventListener('click', function (e) {
+
+  if (headerNav.classList.contains("hidden")) {
+    headerNav.classList.remove("hidden")
+    navButton.src = "./dist/img/close.svg"
+  }
+
+
+  else {
+    headerNav.classList.add("hidden")
+    navButton.src = "./dist/img/hamburger.svg"
+  }
+})
 
 
 
